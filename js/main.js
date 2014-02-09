ff13e8b9c1cb025b3ef2e8b64fc11a563ba12871
Christian N Hernandez unknown student <cnh5@afsconnect1.njit.edu>
2014-02-09 17:32:27 -0500

    added image carousel function

--- js/main.js
+++ js/main.js
@@ -55,3 +55,54 @@
 			}
 		});
 	}
+	
+	//Set up Image Carousel
+	function setImg() {
+		var container = $('#img-container'),
+			nav = $('#product-img').find('nav'),
+			imgList = Object,
+			current = 0,
+			swipeEnabled = false;
+		
+		function buildGallery() {
+			container.html('<div id="img-list"><ul /></div>');
+			imgList = $('#img-list');
+			nav.find('li:first').addClass('active');
+			
+			var arr = '';
+			
+			//For Each Navigation Link
+			nav.find('a').each(function() {
+				var $this = $(this),
+					href = $this.attr('href');
+					
+				//Prepare list item with image source in data attribute
+				arr += '<li data-imgsrc="'+href+'"></li>';
+			});
+			
+			//Append to #img-list
+			imgList.find('ul').append(arr);
+			
+			//Nav Thumbnail Click
+			nav.on('click','a',function(e) {
+				var pos = $(this).parent().index();
+				e.preventDefault();
+				loadImg(pos);
+				if(swipeEnabled) {
+					mySwipe.slide(pos,300);
+				}
+				updateNav(pos);
+			});
+			
+			Modernizr.load({
+			  test: Modernizr.touch && Modernizr.csstransitions,
+			  yep : 'js/swipe.js',
+			  complete : function() {
+					if(w.Swipe) {
+						swipeEnabled = true;
+						buildSwipe();
+					}
+			  }
+			});
+			loadImg(0); //Load initial image
+		}
